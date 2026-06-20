c5e04ad3f0393b67230f3726646661e6963c2163
danielsaggir <danielsaggir@gmail.com>
2026-06-20 12:35:08 +0300

    Refactor task routes: remove unused tasks data import and clean up code formatting

--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const tasks = require("../data/tasks.data");
 const { getAllTasks, createTask } = require("../controllers/tasks.controller");
 const validate = require("../middleware/validate");
- const { createTaskSchema } = require("../schemas/task.schema");
+const { createTaskSchema } = require("../schemas/task.schema");
 const router = express.Router();
 
 router.get("/", getAllTasks);
